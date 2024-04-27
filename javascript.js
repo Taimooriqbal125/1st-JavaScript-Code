0ac1f73955eb2c1d1ae581e2ea71644453d35538
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-04-28 00:27:28 +0500

    Substraction

--- javascript.js
+++ javascript.js
@@ -9,6 +9,12 @@ var num3 = 30;
 var arthemeticOperator = num1 + num2 + num3 ;
 console.log (arthemeticOperator)
 
+// Substraction
 
+var digit1 = 20;
+var digit2 = 40;
+var digit3 = 10;
+var arthemeticOperator = digit1 - digit2 - digit3 ;
+console.log(arthemeticOperator)
 
 
