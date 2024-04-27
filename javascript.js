9826825368f12c6dd464218b3f3c54bf94939cda
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-04-27 21:43:50 +0500

    Addition

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// 1st program using Arthemetic Operators 
+
+
+// Addition 
+
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var arthemeticOperator = num1 + num2 + num3 ;
+console.log (arthemeticOperator)
+
+
+
+
